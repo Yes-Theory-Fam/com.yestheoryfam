818751771ef70f3083caa7eee9e213b5fbc2079f
jamie.legg <jamie.legg@nccgroup.com>
2019-12-17 10:40:49 +0000

    Further restructuring

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Yes Theory community page coming soon.</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
