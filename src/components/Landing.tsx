c712823f43523c63aaf008cad0af205c1e874f3c
Håkon Solbjørg <hakon@solbj.org>
2019-12-19 21:41:02 +0100

    feat: Set up true hot-reloading of React with WDS

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { hot } from 'react-hot-loader';
 
 export interface LandingProps { compiler: string; framework: string; }
 
@@ -7,3 +8,5 @@ export class Landing extends React.Component<LandingProps, {}> {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
+
+export default hot(module)(Landing);
--- webpack.dev.js
+++ webpack.dev.js
@@ -17,6 +17,10 @@ module.exports = {
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
+    devServer: {
+        hot: true,
+        inline:true,
+    },
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
