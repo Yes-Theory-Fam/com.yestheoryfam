25cdd8be1c52ec3baf8540e6cebe4fb7002e66a9
geisterfurz007 <michelvonv@me.com>
2020-02-01 16:28:43 +0100

    Changed alert to a proper error to the console

--- src/components/DiscordAuthenticationCallback/DiscordAuthenticationCallback.tsx
+++ src/components/DiscordAuthenticationCallback/DiscordAuthenticationCallback.tsx
@@ -1,29 +1,31 @@
 import * as React from 'react';
 import '../Redirect.scss';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, Redirect } from 'react-router-dom';
 
 const DiscordAuthenticationCallback = () => {
-    // https://stackoverflow.com/a/43682482/6707985
-    const toEntries = (arr: Array<Array<string>>) => Object.assign({}, ...Array.from(arr, ([k, v]) => ({[k]: v}) ));
-    
     const storedState = localStorage.getItem("auth_state");
     const sentState = new URLSearchParams(window.location.hash).get("state");
-
-    if (storedState !== sentState) {
-        alert("That's bad. Shouldn't have happened. Should also have a better error message than just an alert, really.");
-    }
 
     localStorage.removeItem("auth_state");
 
     const history = useHistory();
     history.push("/");
 
-    return (
-        <div className="discordAuth">
-            <p>You should have been redirected to the main page already!</p>
-            <p>Click <Link to="/">here</Link> to get there.</p>
-        </div>
-    );
+    if (storedState !== sentState) {
+        console.error("Stored state did not equal the one sent by discord. User won't be authenticated!");
+        return <RedirectNotice />
+    }
+
+    // Use the token to do auth here.
+
+    return <RedirectNotice />;
 };
+
+const RedirectNotice = () => {
+    return <div className="discordAuth">
+        <p>You should have been redirected to the main page already!</p>
+        <p>Click <Link to="/">here</Link> to get there.</p>
+    </div>
+}
 
 export default DiscordAuthenticationCallback;
