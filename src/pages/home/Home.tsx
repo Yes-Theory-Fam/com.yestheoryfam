e57d7aa4bcded28829ccb359c677abcf5ea4b55e
geisterfurz007 <michelvonv@me.com>
2020-03-19 00:32:59 +0100

    feat: added Scroll for more hint
    Adds an SVG that is used in that hint
    Adds the HTML and CSS required to create the hint

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import "./Home.scss";
 import cloudBig from "../../assets/cloudBig.png";
 import cloudBot from "../../assets/cloudBot.png";
 import NavBar from "../../components/NavBar/NavBar";
+import ExpandForMore from "../../assets/expand-for-more-yt-blue.svg";
 
 const Content: React.FC = () => {
   return <div className="coming-soon">COMING SOON!</div>;
@@ -24,6 +25,12 @@ const Home: React.FC = () => {
             <img src={cloudBot} className="cloud bot-front" />
             <img src={cloudBig} className="cloud big-front" />
             <img src={cloudBig} className="cloud big-front" />
+            <div className="scroll-for-more">
+              SCROLL FOR MORE
+              <div className="expand-container">
+                <ExpandForMore />
+              </div>
+            </div>
           </div>
           <Content />
         </div>
