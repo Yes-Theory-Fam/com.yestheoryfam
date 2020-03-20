2a92c6a74b2b788796d485d714f5d3d72a6090d6
geisterfurz007 <michelvonv@me.com>
2020-03-20 22:00:53 +0100

    feat: Showcase new component on Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,9 +4,36 @@ import cloudBig from "../../assets/cloudBig.png";
 import cloudBot from "../../assets/cloudBot.png";
 import NavBar from "../../components/NavBar/NavBar";
 import ExpandForMore from "../../assets/expand-for-more-yt-blue.svg";
+import HomeEntry from "../../components/HomeEntry/HomeEntry";
 
 const Content: React.FC = () => {
-  return <div className="coming-soon">COMING SOON!</div>;
+  return (
+    <div style={{ zIndex: 500, width: "100%", background: "white", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly" }}>
+      <HomeEntry
+        headerBlackText="Discover"
+        headerBlueText="empowering stories"
+        imageSide="left"
+      >
+        <p>
+          We believe in authentic, fulfilling and life changing experiences.
+          Therefore, <b>we want to share stories that matter</b> with the world
+          as motivational examples of what can be achieved.
+        </p><br />
+        <p>
+          Read about the fulfilling experiences other YesFam members had, or
+          join and share your own with the world.
+        </p>
+      </HomeEntry>
+      <HomeEntry
+      headerBlackText="Find closest"
+      headerBlueText="meetups"
+      imageSide="right">
+        <p>
+          Wondering how to get involved in this amazing community? We are organising amazing events in many different countries. These events are called <b>FiYESta</b>. Click the button below to find out when the next even will be happening.
+        </p>
+      </HomeEntry>
+    </div>
+  );
 };
 
 const Home: React.FC = () => {
