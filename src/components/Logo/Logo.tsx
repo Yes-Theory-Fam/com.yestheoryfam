d9b4be2819bee89014db6657750755ab6e2c4e56
geisterfurz007 <michelvonv@me.com>
2020-03-21 18:34:38 +0100

    Extracted Logo to seperate component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import './Logo.scss';
+import { Link } from 'react-router-dom';
+
+const Logo: React.FC = () => {
+    return <Link to="/" className="logo">YESTHEORY<b>FAM</b></Link>
+}
+
+export default Logo;
