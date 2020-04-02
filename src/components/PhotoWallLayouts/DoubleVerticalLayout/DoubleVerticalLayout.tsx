02c9c41a0e7ced8930eb490f67bdc863533b74d9
geisterfurz007 <michelvonv@me.com>
2020-04-02 14:07:49 +0200

    feat: new layout for vertical images
    The main reason behind creating this layout is that a batch of 4
    vertical images did not have a fitting layout yet. This leads to an
    infinite loop attempting to find a fitting layout for this case which
    fails to complete.
    Adding this layout that takes at least 2 vertical images and has no
    required horizontal images this problem is solved.

---
+++ src/components/PhotoWallLayouts/DoubleVerticalLayout/DoubleVerticalLayout.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { PhotoWallLayoutProps, LayoutProps } from "../PhotoWallLayoutTypes";
+import PhotoWrapper from "../PhotoWrapper/PhotoWrapper";
+import { splitImages } from "../../../utils";
+
+const DoubleVerticalLayout: React.FC<LayoutProps> = ({ images }) => {
+  const [, chosenVerticals, otherImages] = splitImages(images, 0, 2);
+
+  return (
+    <div className="row layout-row">
+      <PhotoWrapper className="w-33" src={chosenVerticals[0].src} />
+      <div className="column w-33">
+        <PhotoWrapper className="h-50" src={otherImages[0].src} />
+        <PhotoWrapper className="h-50" src={otherImages[1].src} />
+      </div>
+      <PhotoWrapper className="w-33" src={chosenVerticals[1].src} />
+    </div>
+  );
+};
+
+export default DoubleVerticalLayout;
+
+export const props: PhotoWallLayoutProps = {
+  horizontalImages: 0,
+  totalImages: 4,
+  verticalImages: 2
+};
--- src/components/PhotoWallLayouts/index.ts
+++ src/components/PhotoWallLayouts/index.ts
@@ -3,13 +3,15 @@ import OneFourLayout, { props as OFLProps } from './OneFourLayout/OneFourLayout'
 import DoubleRowLayout, { props as DRLProps } from './DoubleRowLayout/DoubleRowLayout';
 import VerticalLeftLayout, { props as VLLProps } from './VerticalLeftLayout/VerticalLeftLayout';
 import DoubleColumnLayout, { props as DCLProps } from './DoubleColumnLayout/DoubleColumnLayout';
+import DoubleVerticalLayout, { props as DVLProps } from './DoubleVerticalLayout/DoubleVerticalLayout';
 
 const layouts: Array<{ LayoutComponent: React.FC<LayoutProps>, props: PhotoWallLayoutProps }> =
     [
         { LayoutComponent: OneFourLayout, props: OFLProps },
         { LayoutComponent: DoubleRowLayout, props: DRLProps },
         { LayoutComponent: VerticalLeftLayout, props: VLLProps },
-        { LayoutComponent: DoubleColumnLayout, props: DCLProps }
+        { LayoutComponent: DoubleColumnLayout, props: DCLProps },
+        { LayoutComponent: DoubleVerticalLayout, props: DVLProps },
     ];
 
 export default layouts;
