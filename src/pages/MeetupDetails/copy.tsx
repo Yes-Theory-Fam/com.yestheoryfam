d397971d6402ac2f2f42112c9de8321b94237673
geisterfurz007 <michelvonv@me.com>
2020-04-03 23:07:39 +0200

    fix: make link to discord a link

--- src/pages/MeetupDetails/copy.tsx
+++ src/pages/MeetupDetails/copy.tsx
@@ -1,4 +1,6 @@
-const step1 = "Join our discord (https://discord.com/yestheory). The server is where most of the planning and conversation takes place. It's a fun place to hang out and once you get into it, it will be hard to leave!";
+import * as React from 'react';
+
+const step1 = <div>Join our discord (<a href="https://discord.com/yestheory" className="inline-blue">https://discord.com/yestheory</a>). The server is where most of the planning and conversation takes place. It's a fun place to hang out and once you get into it, it will be hard to leave!"</div>;
 const step2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat vel est nec facilisis. Vestibulum mollis at nisl auctor condimentum. Vestibulum sed sem mi.";
 const step3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat vel est nec facilisis. Vestibulum mollis at nisl auctor condimentum. Vestibulum sed sem mi.";
 const step4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat vel est nec facilisis. Vestibulum mollis at nisl auctor condimentum. Vestibulum sed sem mi.";
