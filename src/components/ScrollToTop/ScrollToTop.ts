5bc1a8d317e3601d397f392863391b83ae63d3f3
geisterfurz007 <michelvonv@me.com>
2020-04-03 15:49:52 +0200

    fix: scroll to top after page change

---
+++ src/components/ScrollToTop/ScrollToTop.ts
@@ -0,0 +1,15 @@
+import { FC, useEffect } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router';
+
+// Helper component scrolling up the page after route change
+// https://stackoverflow.com/a/54343182/6707985
+const ScrollToTop: FC<RouteComponentProps> = ({ history }) => {
+  useEffect(() => {
+    const unlisten = history.listen(() => window.scrollTo(0, 0));
+    return () => unlisten();
+  });
+
+  return null;
+}
+
+export default withRouter(ScrollToTop);
