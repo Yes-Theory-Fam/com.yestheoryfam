deb3f1a6ad0f18d3bbcc875f44830ba70f9c0c39
Håkon Solbjørg <hakon@solbj.org>
2020-04-16 18:23:26 +0200

    feat: Initial working firebase integration

--- src/pages/BuddyProject/BuddyProject.tsx
+++ src/pages/BuddyProject/BuddyProject.tsx
@@ -1,15 +1,66 @@
 import * as React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import { IoIosArrowDown, IoMdClose } from "react-icons/io";
+import { initDb, buddyProjectSignup, fetchBuddyProjectSignup, BuddyProjectSignup } from "./buddyprojectFirebase";
 import './BuddyProject.scss';
 import Footer from "../../components/Footer/Footer";
 
-const BuddyModal: React.FC<{onClose: () => void}> = ({onClose}) => {
+interface BuddyModal extends BuddyProjectSignup {
+  onClose: () => void;
+}
+
+const BuddyModal: React.FC<BuddyModal> = ({discordUserId, discordUsername, displayName, onClose}) => {
+    const [error, setError] = React.useState("");
+    // This variable starts off as undefined, so that we can initiate a request
+    // to fetch the user. The request will respond with either a BuddyProjectSignup or null.
+    // In other words: currentSignup is `undefined` until we've fetched the current signup
+    // and after that it'll be null or an object.
+    const [currentSignup, setCurrentSignup] = React.useState<BuddyProjectSignup | null | undefined>(undefined);
+
+    React.useEffect(() => {
+      // Don't think this really is the place to do this, but hey time pressure etc.
+      initDb();
+    })
+
+    React.useEffect(() => {
+      if (currentSignup !== undefined) {
+        // Don't try to fetch existing user unless this is undefined, which
+        // is its initial value.
+        return;
+      }
+
+      fetchBuddyProjectSignup(discordUserId)
+        .then(setCurrentSignup)
+        .catch(e => setError(e));
+    });
+
     return (
       <div className="buddy-project-modal">
         <div style={{ height: "100%", width: "100%" }} className="column-center">
           <CloseButton onClick={onClose} />
           <div className="upload-header">Find a stranger, discover a friend.</div>
+          {/* @ToDo: if not logged in, make 'em log in */}
+          <div>
+            <h3>Hi {displayName}!</h3>
+            <p>Or should I call you {discordUsername}?</p>
+            { currentSignup === undefined && <> Hang on a sec... I'm checking if you're already signed up. </>}
+            { (currentSignup !== undefined && currentSignup !== null) && <> You're already signed up silly! </> }
+            { currentSignup === null &&
+            <>
+              <p>Click this button to sign up for the buddy project
+                and we'll assign you a buddy real soon! (add more details about when etc)
+              </p>
+              <p>After you've been assigned a buddy, you will be able to see who it is and how to contact them here.</p>
+              <p>Feel free to check back on this page for any updates.</p>
+              { error !== "" && <div>Seems like something went wrong! 🤔</div>}
+              <form onSubmit={(e) => {
+                e.preventDefault();
+                buddyProjectSignup(discordUsername, displayName, discordUserId)
+              }}>
+                <button type="submit" className="button buddy-project-entry">GIVE ME A BUDDY</button>
+              </form>
+            </>}
+          </div>
         </div>
       </div>
     );
@@ -51,7 +102,12 @@ const BuddyProject: React.FC<{}> = () => {
     return (
       <>
         <NavBar fixed classNames={showModal ? "blur" : ""}/>
-        {showModal && <BuddyModal onClose={() => setShowModal(false)} />}
+        {showModal && <BuddyModal
+          onClose={() => setShowModal(false)}
+          discordUsername="YESBOT#0001"
+          displayName="YesBot"
+          discordUserId="614101602046836776"
+          />}
         <div className={`column-center ${showModal ? "blur" : ""}`}>
           <div className="column-center photo-wall-top">
             <div></div>{/* div required here to have space-between sort everything out */}
