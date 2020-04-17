12e1170d82adfdf9c106cc44911ed439f55e67bf
geisterfurz007 <michelvonv@me.com>
2020-04-17 12:44:02 +0200

    chore: formatting, readding Groupchats and redirect to home

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import DiscordAuthenticationRequest from "./components/DiscordAuthenticationRequest/DiscordAuthenticationRequest";
 import DiscordAuthenticationCallback from "./components/DiscordAuthenticationCallback/DiscordAuthenticationCallback";
 import Home from "./pages/home/Home";
@@ -28,52 +28,65 @@ const DiscordApi = () => {
   const api = axios.create({
     baseURL: "https://discordapp.com/api/",
     timeout: 3000,
-    headers: {"Authorization":localStorage.getItem("access_token")}
-  })
+    headers: { Authorization: localStorage.getItem("access_token") },
+  });
   return api;
-}
+};
 
-const getInitialUser = async () : Promise<IDiscordUser> => {
-  const { id, username, avatar, discriminator, email } = await DiscordApi().get("users/@me");
+const getInitialUser = async (): Promise<IDiscordUser> => {
+  const { id, username, avatar, discriminator, email } = await DiscordApi().get(
+    "users/@me"
+  );
   return {
     username,
     avatar,
     id,
     discriminator,
-    email
-  }
-}
+    email,
+  };
+};
 
 const App = () => {
-  
-  const [ user, setUser ] = React.useState<undefined | IDiscordUser>(undefined);
+  const [user, setUser] = React.useState<undefined | IDiscordUser>(undefined);
   getInitialUser().then(setUser);
-  
+
   return (
     <Router>
       <ToastContainer />
       <ScrollToTop />
-      <UserContext.Provider value={{user, setUser}}>
-      <Switch>
-        
-        <Route path="/" exact component = {Home} />
-        <Route path="/auth/discord" exact component = {DiscordAuthenticationRequest} />
-        <Route path="/oauth/redirect" exact component = {DiscordAuthenticationCallback} />
-        <Route path="/blog" exact component = {BlogOverview} />
-        <Route path="/photowall" exact component = {PhotoWall} />
-        <Route path="/meetups" render={({ match: { url } }) => (
-          <>
-            <Route path={`${url}/:id`} component={MeetupDetails} />
-            <Route path={`${url}/`} exact>
-              <Meetups />
-            </Route>
-          </>)}/>
-      </Switch>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route
+            path="/auth/discord"
+            exact
+            component={DiscordAuthenticationRequest}
+          />
+          <Route
+            path="/oauth/redirect"
+            exact
+            component={DiscordAuthenticationCallback}
+          />
+          <Route path="/blog" exact component={BlogOverview} />
+          <Route path="/photowall" exact component={PhotoWall} />
+          <Route path="/groupchats" exact component={Groupchats} />
+          <Route
+            path="/meetups"
+            render={({ match: { url } }) => (
+              <>
+                <Route path={`${url}/:id`} component={MeetupDetails} />
+                <Route path={`${url}/`} exact>
+                  <Meetups />
+                </Route>
+              </>
+            )}
+          />
+          <Redirect path="/" to="/home" />
+        </Switch>
       </UserContext.Provider>
-
     </Router>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
