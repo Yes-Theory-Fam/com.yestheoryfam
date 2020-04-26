43c079a4dda4122ff527b5d75ce18659a4a59079
James Dimonaco <James@Jamess-MacBook-Pro.local>
2020-04-26 14:54:21 +0100

    Added Icon links to facebook innsta and youtube

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./Footer.scss";
 import Logo from "../Logo/Logo";
+import { FaFacebookF, FaInstagram, FaYoutube }from 'react-icons/fa';
 
 const Footer: React.FC<{}> = () => {
   return (
@@ -14,6 +15,8 @@ const Footer: React.FC<{}> = () => {
         <a href="https://seekdiscomfort.com" target="_blank">
           Seek Discomfort
         </a>
+      </div>
+      <div className="iconLinks"> <a className="facebookIcon"href="https://www.facebook.com/groups/yesfam/" target="_blank"><FaFacebookF /> </a> <a className="instagramIcon" href="https://www.instagram.com/yestheory/" target="_blank"><FaInstagram/> </a> <a className="youtubeIcon" href="https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA" target="_blank"><FaYoutube/> </a>
       </div>
       <div className="footer-copyright">
         &#169; {new Date().getFullYear()} YESTHEORYFAM
