eb66f7d0f264ae9cbac990e5357b4d4851ae74b9
geisterfurz007 <michelvonv@me.com>
2020-05-01 20:05:14 +0200

    feat: redone photowall with grid-layout

--- src/components/PhotoWallLayouts/DoubleColumnLayout/DoubleColumnLayout.tsx
+++ src/components/PhotoWallLayouts/DoubleColumnLayout/DoubleColumnLayout.tsx
@@ -3,19 +3,17 @@ import PhotoWrapper from "../PhotoWrapper/PhotoWrapper";
 import { PhotoWallLayoutProps, LayoutProps } from "../PhotoWallLayoutTypes";
 import { splitImages } from "../../../utils";
 
+import "./DoubleColumnLayout.scss";
+
 const DoubleColumnLayout: React.FC<LayoutProps> = ({ images }) => {
   const [selectedHorizontal, , otherImages] = splitImages(images, 2, 0);
 
   return (
-    <div className="row layout-row">
-      <div className="column w-50">
-        <PhotoWrapper className="h-66" src={otherImages[0].src} />
-        <PhotoWrapper className="h-33" src={selectedHorizontal[0].src} />
-      </div>
-      <div className="column w-50">
-        <PhotoWrapper className="h-33" src={selectedHorizontal[1].src} />
-        <PhotoWrapper className="h-66" src={otherImages[1].src} />
-      </div>
+    <div className="dc-layout layout-row">
+      <PhotoWrapper className="tall-1" src={otherImages[0].src} />
+      <PhotoWrapper src={selectedHorizontal[0].src} />
+      <PhotoWrapper src={selectedHorizontal[1].src} />
+      <PhotoWrapper className="tall-2" src={otherImages[1].src} />
     </div>
   );
 };
--- src/components/PhotoWallLayouts/VerticalLeftLayout/VerticalLeftLayout.tsx
+++ src/components/PhotoWallLayouts/VerticalLeftLayout/VerticalLeftLayout.tsx
@@ -4,6 +4,8 @@ import { PhotoWallLayoutProps, LayoutProps } from "../PhotoWallLayoutTypes";
 import PhotoWrapper from "../PhotoWrapper/PhotoWrapper";
 import { splitImages } from "../../../utils";
 
+import "./VerticalLeftLayout.scss";
+
 const VerticalLeftLayout: React.FC<LayoutProps> = ({ images }) => {
   const [[chosenHorizontal], [chosenVertical], otherImages] = splitImages(
     images,
@@ -12,15 +14,11 @@ const VerticalLeftLayout: React.FC<LayoutProps> = ({ images }) => {
   );
 
   return (
-    <div className="row layout-row">
-      <PhotoWrapper className="w-33" src={chosenVertical.src} />
-      <div className="column w-66">
-        <PhotoWrapper className="h-50" src={chosenHorizontal.src} />
-        <div className="row h-50">
-          <PhotoWrapper className="w-50" src={otherImages[0].src} />
-          <PhotoWrapper className="w-50" src={otherImages[1].src} />
-        </div>
-      </div>
+    <div className="vl-layout layout-row">
+      <PhotoWrapper className="tall" src={chosenVertical.src} />
+      <PhotoWrapper className="long" src={chosenHorizontal.src} />
+      <PhotoWrapper src={otherImages[0].src} />
+      <PhotoWrapper src={otherImages[1].src} />
     </div>
   );
 };
