b0e3c219b3e21d67fc8772a7fd5589172d2df789
geisterfurz007 <michelvonv@me.com>
2020-05-06 01:22:50 +0200

    feat: correct images for everyone

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,80 +1,73 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "./index.html"),
-    filename: "./index.html"
+  template: path.join(__dirname, "./index.html"),
+  filename: "./index.html",
 });
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    mode: "development",
-    optimization: {
-        splitChunks: {
-            // include all types of chunks
-            chunks: 'all'
-        }
+  mode: "development",
+  optimization: {
+    splitChunks: {
+      // include all types of chunks
+      chunks: "all",
     },
+  },
 
-    output: {
-        publicPath: "/",
-    },
-    
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "inline-source-map",
-    
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline:true,
-    },
+  output: {
+    publicPath: "/",
+  },
 
-    entry: [
-        'react-hot-loader/patch',
-        './src/index',
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "inline-source-map",
+
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+  },
+
+  entry: ["react-hot-loader/patch", "./src/index"],
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.s?css$/,
+        loader: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+        },
+      },
     ],
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js"]
-    },
-
-    module: {
-
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    }
-                ]
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            },
-            {
-                test: /\.s?css$/,
-                loader: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack']
-            },
-            {
-                test: /\.png$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 8192,
-                }
-            }
-        ]
-    },
-    plugins: [
-        htmlWebpackPlugin,
-        new Dotenv(),
-    ]
+  },
+  plugins: [htmlWebpackPlugin, new Dotenv()],
 };
