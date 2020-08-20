388a32fe0fd84751c04a3640cbdf46d12a8408dc
geisterfurz007 <michelvonv@me.com>
2020-08-20 10:07:34 +0200

    feat: write-a-blog first, very rough, half done version

---
+++ src/pages/Playground/copy.ts
@@ -0,0 +1,3 @@
+const suggestions = ["All images should have a non-empty caption", "Every idk how many characters Matej likes characters you should insert an image or other kind of media"];
+
+export default suggestions;
